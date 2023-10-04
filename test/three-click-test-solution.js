749975248c53a72768780632d23ba079bbd4185c
Marc Wright <marc.wright@gmail.com>
2023-10-04 09:49:59 -0500

    refactor first 3 tests

--- test/three-click-test-solution.js
+++ test/three-click-test-solution.js
@@ -24,116 +24,84 @@ const assert = require("assert");
 
 //   main();
 
-const main = async () => {
+const main = () => {
   let driver = new selenium.Builder().forBrowser("chrome").build();
   driver.get("http://localhost:3000/");
-  // async function main() {
-  // after(function () {
-  //   // shut down the Selenium web driver after all the tests.
-  //   driver.quit();
-  // });
 
-  describe("initial state 3 test", async () => {
+  describe("initial state 3 test", () => {
     it("should have the top message say: Three Button Game.", async () => {
       const selection = await selenium.By.id("top-message");
-      const element = await driver.findElement(selection);
-      const text = await element.getText();
-      console.log("THIS IS THE ETXT", text);
-      var expected = "Three Button Game";
-      //       assert.equal(txt, expected);
-      assert.equal(text, expected);
+      const text = await driver.findElement(selection).getText();
+      assert.equal(text, "Three Button Game");
+    });
+
+    it("should have the bottom message say: Click those buttons!", async () => {
+      const selection = await selenium.By.id("winner");
+      const text = await driver.findElement(selection).getText();
+      assert.equal(text, "Click those buttons!");
+    });
+
+    it("each button should say click me", async () => {
+      let foundNum = 0;
+      for (var i = 1; i <= 3; i++) {
+        const buttonId = "button" + i;
+
+        const selection = await selenium.By.id(buttonId);
+        const text = await driver.findElement(selection).getText();
+        assert.equal(text, "click me");
+        foundNum++;
+      }
+      await driver.close();
     });
   });
-
-  //   driver
-  //     .findElement(By.id("top-message"))
-  //     .getText()
-  //     .then(function (txt) {
-  //       var expected = "Three Button Game";
-  //       assert.equal(txt, expected);
-  //     })
-  //     .then(done);
-  // });
-
-  // it("should have the bottom message say: Click those buttons!", function (done) {
-  //   driver
-  //     .findElement(By.id("winner"))
-  //     .getText()
-  //     .then(function (txt) {
-  //       var expected = "Click those buttons!";
-  //       assert.equal(txt, expected);
-  //     })
-  //     .then(done);
-  // });
-
-  // it("each button should say click me", function (done) {
-  //   var foundNum = 0;
-  //   for (var i = 1; i <= 3; i++) {
-  //     var buttonId = "button" + i;
-  //     driver
-  //       .findElement(By.id(buttonId))
-  //       .getText()
-  //       .then(function (txt) {
-  //         assert.equal(txt, "click me");
-  //       })
-  //       .then(function () {
-  //         foundNum++;
-  //         if (foundNum == 3) {
-  //           done();
-  //         }
-  //       });
-  //   }
-  // });
-  // }
-  // });
-
-  // describe(
-  //   "button clicking",
-  //   await function () {
-  //     it("should not win if one button is clicked", function (done) {
-  //       driver.findElement(By.id("button1")).click();
-  //       driver
-  //         .findElement(By.id("button1"))
-  //         .getText()
-  //         .then(function (txt) {
-  //           assert.equal(txt, "clicked");
-  //         });
-  //       driver
-  //         .findElement(By.id("winner"))
-  //         .getText()
-  //         .then(function (txt) {
-  //           assert.equal(txt, "Click those buttons!");
-  //         })
-  //         .then(done);
-  //     });
-
-  //     it("should display 'clicked' for all buttons", function (done) {
-  //       var numClicked = 0;
-  //       for (var i = 1; i <= 3; i++) {
-  //         var id = "button" + i;
-  //         driver.findElement(By.id(id)).click();
-  //         driver
-  //           .findElement(By.id(id))
-  //           .getText()
-  //           .then(function (txt) {
-  //             assert.equal(txt, "clicked");
-  //           })
-  //           .then(function () {
-  //             numClicked++;
-  //             if (numClicked === 3) {
-  //               driver
-  //                 .findElement(By.id("winner"))
-  //                 .getText()
-  //                 .then(function (txt) {
-  //                   assert.equal(txt, "You win!");
-  //                 })
-  //                 .then(done);
-  //             }
-  //           });
-  //       }
-  //     });
-  //   }
-  // );
 };
+
+// describe(
+//   "button clicking",
+//   await function () {
+//     it("should not win if one button is clicked", function (done) {
+//       driver.findElement(By.id("button1")).click();
+//       driver
+//         .findElement(By.id("button1"))
+//         .getText()
+//         .then(function (txt) {
+//           assert.equal(txt, "clicked");
+//         });
+//       driver
+//         .findElement(By.id("winner"))
+//         .getText()
+//         .then(function (txt) {
+//           assert.equal(txt, "Click those buttons!");
+//         })
+//         .then(done);
+//     });
+
+//     it("should display 'clicked' for all buttons", function (done) {
+//       var numClicked = 0;
+//       for (var i = 1; i <= 3; i++) {
+//         var id = "button" + i;
+//         driver.findElement(By.id(id)).click();
+//         driver
+//           .findElement(By.id(id))
+//           .getText()
+//           .then(function (txt) {
+//             assert.equal(txt, "clicked");
+//           })
+//           .then(function () {
+//             numClicked++;
+//             if (numClicked === 3) {
+//               driver
+//                 .findElement(By.id("winner"))
+//                 .getText()
+//                 .then(function (txt) {
+//                   assert.equal(txt, "You win!");
+//                 })
+//                 .then(done);
+//             }
+//           });
+//       }
+//     });
+//   }
+// );
 
 main();
